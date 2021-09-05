7ecf2e878c2c4465ec0e9e76290ca8e13cfe4e06
Evelyn-zj <evelyn.zj@sjtu.edu.cn>
2021-09-05 16:18:23 -0400

    704 solution

---
+++ Problems/704.cpp
@@ -0,0 +1,29 @@
+class Solution 
+{
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int begin, end, middle, pos;
+        begin = 0;
+        end = nums.size() - 1;
+        pos = -1;
+        while (begin <= end)
+        {
+            middle = (begin + end) / 2;
+            if (nums[middle] == target)
+            {
+                pos = middle;
+                break;
+            }
+            else if (nums[middle] < target)
+            {
+                begin = middle + 1;
+            }
+            else
+            {
+                end = middle - 1;
+            }
+        }
+        return pos;
+    }
+};
