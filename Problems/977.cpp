9853e0a79babe0f92bb4e8738d0fce1e4ed387b2
Evelyn-zj <evelyn.zj@sjtu.edu.cn>
2021-09-16 23:05:51 -0400

    977 solution

--- Problems/977.cpp
+++ Problems/977.cpp
@@ -3,12 +3,11 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
         vector<int> result(nums.size());
         int i = 0, j = nums.size() - 1;
-        for (int k = nums.size() - 1; k >= 0; --k){
-            if (abs(nums[i]) > abs(nums[j])){
+        for (int k = nums.size() - 1; k >= 0; --k) {
+            if (abs(nums[i]) > abs(nums[j])) {
                 result[k] = nums[i] * nums[i];
                 i++;
-            }
-            else{
+            } else {
                 result[k] = nums[j] * nums[j];
                 j--;
             }
