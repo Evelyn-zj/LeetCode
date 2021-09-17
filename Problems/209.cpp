5cee25c83db5bdbb17b271d1986fc7b8d2332e93
Evelyn-zj <evelyn.zj@sjtu.edu.cn>
2021-09-16 23:04:37 -0400

    209 solution

--- Problems/209.cpp
+++ Problems/209.cpp
@@ -5,21 +5,20 @@ public:
         min = nums.size() + 1;
         n = 0;
         sum = 0;
-        for (int i = 0; i < nums.size(); ++i){
+        for (int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
             n++;
-            while (sum >= target){
-                if (n < min){
+            while (sum >= target) {
+                if (n < min) {
                     min = n;
                 }
                 sum = sum - nums[i + 1 - n];
                 n--;
             }
         }
-        if (min == nums.size() + 1){
+        if (min == nums.size() + 1) {
             return 0;
-        }
-        else{
+        } else {
             return min;
         }
     }
