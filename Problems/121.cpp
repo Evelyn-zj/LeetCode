dbe4cfaf7f6ef666dbbdbf7d7c963df844a8fa5a
Evelyn-zj <evelyn.zj@sjtu.edu.cn>
2022-01-14 15:15:44 -0500

    121 solution

---
+++ Problems/121.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() == 0) {
+            return 0;
+        }
+        int min = prices[0];
+        int max = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] - min > max) {
+                max = prices[i] - min;
+            }
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+        }
+        return max;
+    }
+};
